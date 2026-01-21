da78c3cd87011a308a4b0684de162c34b707161c
cassandratay <cassandratayzx@gmail.com>
2026-01-21 18:10:12 +0800

    Level-0: Renamed chatbot, added greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Theo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Theo.java
@@ -0,0 +1,7 @@
+public class Theo {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Theo\nWhat can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
