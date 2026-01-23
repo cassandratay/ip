5dd594f716efbebacfa4cadf8df6dc538861b7e4
cassandratay <cassandratayzx@gmail.com>
2026-01-23 17:38:57 +0800

    Level-1: Added echo, added exit upon bye

--- src/main/java/Theo.java
+++ src/main/java/Theo.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Theo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello, I'm Theo\nWhat can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+
+            }
+        }
     }
 }
